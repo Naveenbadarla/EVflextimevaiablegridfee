315578bdb9a7797eb137fd9be06b611197768974
Naveenbadarla <naveen.badarla@eon.com>
2025-12-02 22:32:28 +0100

    Update EVflexTVGF.py

--- EVflexTVGF.py
+++ EVflexTVGF.py
@@ -215,7 +215,7 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-st.markdown("<h1 style='text-align:center; font-family:Trebuchet MS, Helvetica, sans-serif; font-weight:700; color:#E2000F; letter-spacing:1px; margin-top:12px;'>E.ON AI<span style='color:white;'>X</span> — AI eXcellence Initiative</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align:center; font-family:Trebuchet MS, Helvetica, sans-serif; font-weight:700; color:#E2000F; letter-spacing:1px; margin-top:12px;'>E.ON AI<span style='color:white;'>X</span> — AI e<span style='color:white;'>X</span>cellence Initiative</h1>", unsafe_allow_html=True)
 
 from streamlit.components.v1 import html
 
